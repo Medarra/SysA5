fcfba2a1e581b07b1b2fe3cfd522e144f62d7079
Curtis Wentzlaff <cwentzlaff4749@conestogaa.on.ca>
2025-04-11 21:53:20 -0400

    Added the fork and process change to dp-1.

--- DP-1/src/dp-1.c
+++ DP-1/src/dp-1.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h>
 
 #include "../../Common/src/buffer.c"
 #include "../../Common/src/semaphore.c"
@@ -15,7 +16,6 @@
 int main(void)
 {   
     int shmID; // memory’s ID
-
     if((shmID = shmget(SEM_KEY,sizeof(CircularBuffer),0)) == -1) {
         // memory doesn’t exist – create it
         shmID = shmget(SEM_KEY, sizeof(CircularBuffer), (IPC_CREAT | 0660));
@@ -27,11 +27,23 @@ int main(void)
 
     CircularBuffer* buffer = shmat(shmID, NULL, 0);
 
+    // Check if this process is a fork, changing the process to DP-2 if it is
+    int result;             // used for testing the forking process and detaching the semaphore
+    if (result == 0) {
+        char idString[11] = { '\0' };
+        sprintf(idString,"%d", shmID);
+        execl("./../DP-2/bin/dp-2", "dp-2", idString, NULL);
+    }
+
+    // Attempt to fork the process
+    result = fork();
+    if (result < 0) {
+        //error
+    }
+
     // Check for existance of a semaphore
     // If it doesn't exist, create the semaphore
     // Do we need two semaphores? One for buffer, one for write index?
-
-    // Through fork call, launch DP-2 (with shmID as a passed command line argument)
     
     // Set up signal handler to respond to (or listen for) SIGINT signal
 
@@ -41,4 +53,6 @@ int main(void)
     //      Sleep for two seconds
 
     // Detach from shared memory and exit
+    shmdt(buffer);
+    return 0;
 }
